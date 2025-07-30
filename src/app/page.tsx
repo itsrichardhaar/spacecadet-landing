e0b3e8ad44488064ec65c7bc6237b48e28675e2f
Richard Haar <35735904+itsrichardhaar@users.noreply.github.com>
2025-07-30 11:34:07 -0400

    Update tab title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,30 @@
 import Image from "next/image";
+import Head from "next/head";
 import styles from "../styles/home.module.css";
 
 export default function Home() {
   return (
-    <section className={styles.home}>
-      <Image
-        src="/space-logo.png"
-        alt="Spacecadet Logo"
-        width={300}
-        height={300}
-        className={styles.logo}
-      />
-      <h1>Welcome to Spacecadet.io</h1>
-      <p>Your mission control for the modern web</p>
-    </section>
+    <>
+      <Head>
+        <title>Spacecadet | Ready. Set. Launch.</title>
+        <meta name="description" content="Your mission control for the modern web." />
+      </Head>
+
+      <section className={styles.home}>
+        <Image
+          src="/space-logo.png"
+          alt="Spacecadet Logo"
+          width={300}
+          height={300}
+          className={styles.logo}
+        />
+        <h1>Welcome to Spacecadet.io</h1>
+        <p>Your mission control for the modern web</p>
+      </section>
+    </>
   );
 }
+
 
 
 
